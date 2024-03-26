6e2a1e1ddabe5e05c219a0d74a6fbbb0548b27d5
NatalySv <136372155+NatalySv@users.noreply.github.com>
2024-03-26 11:50:29 +0200

    Update 2-form.html, create & update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,40 @@
-console.log('Form');
+const feedbackForm = document.querySelector('.feedback-form');
+const email = feedbackForm.querySelector('input[type="email"]');
+const message = feedbackForm.querySelector('textarea[name="message"]');
+const formKey = 'feedback-form-state';
+
+feedbackForm.addEventListener('input', onInputTextarea);
+feedbackForm.addEventListener('submit', handleSubmit);
+backLocalstorage();
+
+function onInputTextarea(event) {
+  const infoFromForm = {
+    email: email.value,
+    message: message.value,
+  };
+
+  localStorage.setItem(formKey, JSON.stringify(infoFromForm));
+}
+
+function backLocalstorage() {
+  const savedForm = JSON.parse(localStorage.getItem(formKey));
+  if (savedForm) {
+    email.value = savedForm.email;
+    message.value = savedForm.message;
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  if (!email.value || !message.value) {
+    alert('All form fields must be filled in');
+  } else {
+    console.log(
+      `Data feedbackForm: `,
+      JSON.parse(localStorage.getItem(formKey))
+    );
+    localStorage.removeItem(formKey);
+    feedbackForm.reset();
+  }
+}
